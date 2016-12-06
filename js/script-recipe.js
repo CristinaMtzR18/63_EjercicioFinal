6d9c2a55a85a3e89692ef9e9c8c25aedb1260c2a
CristinaMtzR18 <cris.mtz.r18@gmail.com>
2016-12-06 12:43:25 -0600

    Etapa 8

--- js/script-recipe.js
+++ js/script-recipe.js
@@ -2,4 +2,13 @@ $(document).ready( function(){
 	console.log("Página Lista");
 	$(".icon-arrow-left-alt").show();
 	$(".icon-menu").hide();
+
+	$( ".js-show-recipe" ).click(function() {
+		$('.page').removeClass('make');
+  	});
+
+  	$( ".js-show-make" ).click(function() {
+		$('.page').addClass('make');
+  	});
 });
+
