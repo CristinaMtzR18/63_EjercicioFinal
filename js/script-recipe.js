821d607c009946938932ea6412f32e6618218537
CristinaMtzR18 <cris.mtz.r18@gmail.com>
2016-12-06 12:55:10 -0600

    Etapa 9

--- js/script-recipe.js
+++ js/script-recipe.js
@@ -5,10 +5,14 @@ $(document).ready( function(){
 
 	$( ".js-show-recipe" ).click(function() {
 		$('.page').removeClass('make');
+		$('.js-show-recipe').addClass('active');
+		$('.js-show-make').removeClass('active');
   	});
 
   	$( ".js-show-make" ).click(function() {
 		$('.page').addClass('make');
+		$('.js-show-make').addClass('active');
+		$('.js-show-recipe').removeClass('active');
   	});
 });
 
